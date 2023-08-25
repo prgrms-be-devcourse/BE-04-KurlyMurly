49b3fbe8995c2fdce3166acfc9290af40a065a21
2jie0516 <dhtpgks10044@gmail.com>
2023-08-25 21:29:15 +0900

    테스트용 pr

--- src/main/java/com/devcourse/kurlymurly/KurlyMurlyApplication.java
+++ src/main/java/com/devcourse/kurlymurly/KurlyMurlyApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KurlyMurlyApplication {
     public static void main(String[] args) {
         SpringApplication.run(KurlyMurlyApplication.class, args);
+        System.out.println("hello world");
     }
 }
