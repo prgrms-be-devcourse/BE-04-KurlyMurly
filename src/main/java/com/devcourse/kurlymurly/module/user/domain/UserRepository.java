1e82c36018c9ac558c2c5d84566783becaa0ba96
2jie0516 <dhtpgks10044@gmail.com>
2023-08-31 00:17:51 +0900

    [KM-117] 수정:유저 저장소에서 @Repository 어노테이션 제거

--- src/main/java/com/devcourse/kurlymurly/module/user/domain/UserRepository.java
+++ src/main/java/com/devcourse/kurlymurly/module/user/domain/UserRepository.java
@@ -3,7 +3,6 @@ package com.devcourse.kurlymurly.module.user.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     boolean existsByEmail(String email);
 
