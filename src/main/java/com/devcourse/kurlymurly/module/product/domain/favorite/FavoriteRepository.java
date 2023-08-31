b273a2056e2c3614218dc7a72a7caf5548841ac7
Hejow <gmlwh124@naver.com>
2023-09-01 02:44:22 +0900

    [KM-151] 추가: 찜 조회하기 구현

--- src/main/java/com/devcourse/kurlymurly/module/product/domain/favorite/FavoriteRepository.java
+++ src/main/java/com/devcourse/kurlymurly/module/product/domain/favorite/FavoriteRepository.java
@@ -1,9 +1,15 @@
 package com.devcourse.kurlymurly.module.product.domain.favorite;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     Optional<Favorite> findByUserIdAndProductId(Long userId, Long productId);
+
+    @Query("SELECT f FROM Favorite f JOIN FETCH f.product WHERE f.userId = :userId ORDER BY f.createAt")
+    List<Favorite> findAllByUserId(@Param(value = "userId") Long userId);
 }
