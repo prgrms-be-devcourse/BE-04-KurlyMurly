eae87442fbf1ee8e1cea7c82d085df563b44f1e5
sujjangOvO <ssu7038@yu.ac.kr>
2023-09-01 03:41:43 +0900

    수정: List 객체에 Optional 제거

--- src/main/java/com/devcourse/kurlymurly/module/order/domain/OrderSupportJpaRepository.java
+++ src/main/java/com/devcourse/kurlymurly/module/order/domain/OrderSupportJpaRepository.java
@@ -3,9 +3,8 @@ package com.devcourse.kurlymurly.module.order.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderSupportJpaRepository extends JpaRepository<OrderSupport, Long> {
-    Optional<List<OrderSupport>> findAllByUserId(Long userId);
-    Optional<OrderSupport> findByOrderId(Long orderId);
+    List<OrderSupport> findAllByUserId(Long userId);
+    List<OrderSupport> findByOrderId(Long orderId);
 }
