7eb0e1df2c994b1ef252e87f4f2d14843f237a15
Hejow <gmlwh124@naver.com>
2023-09-05 01:22:48 +0900

    [KM-178] 추가: 프론트 환경 구현

---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <div>
+        <h1>Kurly</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
