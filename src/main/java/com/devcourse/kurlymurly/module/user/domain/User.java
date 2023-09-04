b3e206ef72d681824f135615d6ec09d362552ff5
2jie0516 <dhtpgks10044@gmail.com>
2023-09-04 15:48:02 +0900

    [KM-190] 수정:회원 도메인 엔티티 코드 순서 변경

--- src/main/java/com/devcourse/kurlymurly/module/user/domain/User.java
+++ src/main/java/com/devcourse/kurlymurly/module/user/domain/User.java
@@ -7,37 +7,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
-import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
 @Entity
-@DynamicUpdate
 @Table(name = "users")
 public class User extends BaseEntity {
-    protected User() {
-    }
-
-    public User(String name, String loginId, String password, String email, UserInfo info, String phoneNumber) {
-        this.name = name;
-        this.loginId = loginId;
-        this.password = password;
-        this.tier = Tier.THE_PURPLE;
-        this.email = email;
-        this.info = info;
-        this.payPassword = null;
-        this.phoneNumber = phoneNumber;
-        this.role = Role.USER;
-        this.status = UserStatus.NORMAL;
-    }
-
-    public void update(String name, String password, String email, String sex, Date birth, String phoneNumber) {
-        this.name = name;
-        this.password = password;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        info.update(birth,sex);
-    }
 
     public enum Role {USER, ADMIN}
 
@@ -49,10 +25,6 @@ public class User extends BaseEntity {
         LAVENDER,
         WHITE,
         FRIENDS;
-    }
-
-    public boolean isEqualPassword(String password) {
-        return this.password.equals(password);
     }
 
     @Column(nullable = false, length = 10)
@@ -96,23 +68,31 @@ public class User extends BaseEntity {
         return role;
     }
 
-    public UserInfo getInfo() {
-        return info;
+    protected User() {
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setName(String name) {
+    public User(String name, String loginId, String password, String email, UserInfo info, String phoneNumber) {
         this.name = name;
+        this.loginId = loginId;
+        this.password = password;
+        this.tier = Tier.THE_PURPLE;
+        this.email = email;
+        this.info = info;
+        this.payPassword = null;
+        this.phoneNumber = phoneNumber;
+        this.role = Role.USER;
+        this.status = UserStatus.NORMAL;
+    }
+
+    public void update(String name, String password, String email, String sex, Date birth, String phoneNumber) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        info.update(birth, sex);
+    }
+
+    public boolean isEqualPassword(String password) {
+        return this.password.equals(password);
     }
 }
