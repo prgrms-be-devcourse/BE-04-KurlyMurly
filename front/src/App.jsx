8f491c4490177bd1806b4c8ca903638b66b24fb4
2jie0516 <dhtpgks10044@gmail.com>
2023-09-09 20:28:23 +0900

    [KM-184] 추가:장바구니 페이지 구현

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home, SignUp } from './pages';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Home, SignUp} from './pages';
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/sign-up" element={<SignUp/>}/>
+                <Route path="/Cart" element={<Cart/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
