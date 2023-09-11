3290c83730cc7eed561b82c73b865426c082c801
2jie0516 <dhtpgks10044@gmail.com>
2023-09-11 16:59:47 +0900

    [KM-183] 수정:주문서 출력 페이지 라우팅 설정

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Home, SignUp} from './pages';
 import Cart from "./pages/Cart";
 import Favorites from "./pages/Favorites";
+import Receipt from "./pages/Receipt";
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
                 <Route path="/sign-up" element={<SignUp/>}/>
                 <Route path="/cart" element={<Cart/>}/>
                 <Route path="/favorites" element={<Favorites/>}/>
+                <Route path="/receipt" element={<Receipt/>}/>
             </Routes>
         </BrowserRouter>
     );
