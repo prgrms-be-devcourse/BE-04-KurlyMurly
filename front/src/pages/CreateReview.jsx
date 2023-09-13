aa686ffbf9fe11c5c52662052fbf80fcf1ec5d92
sujjangOvO <ssu7038@yu.ac.kr>
2023-09-13 14:44:01 +0900

    수정: 불필요한 React import 제거

--- front/src/pages/CreateReview.jsx
+++ front/src/pages/CreateReview.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function CreateReview() {
     const [product, setProduct] = useState({
--- front/src/pages/Support.jsx
+++ front/src/pages/Support.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function Support() {
     const [supportType, setSupportType] = useState('');
