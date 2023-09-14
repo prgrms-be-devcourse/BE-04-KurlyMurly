4d14542f53f7a97d6329690b2b4a3bd33f0e8e0d
2jie0516 <dhtpgks10044@gmail.com>
2023-09-14 14:56:04 +0900

    [KM-191] 수정:주문서 , 장바구니 페이지에 axios 적용 및 코드 수정

--- front/src/pages/Cart.jsx
+++ front/src/pages/Cart.jsx
@@ -1,81 +1,75 @@
-import { useState } from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 function Cart() {
-  const [cart, setCart] = useState({
-    cartId: '',
-    isIncrease: '',
-  });
-
+  const navigate = useNavigate();
   const [items, setItems] = useState([
-    { cartId: 1, productId: 1, name: '상품 1', price: 10000, quantity: 1 },
-    { cartId: 1, productId: 2, name: '상품 2', price: 15000, quantity: 1 },
-    { cartId: 1, productId: 3, name: '상품 3', price: 20000, quantity: 1 },
+    {cartId: 1, productId: 1, name: '상품 1', price: 10000, quantity: 1},
+    {cartId: 2, productId: 2, name: '상품 2', price: 15000, quantity: 1},
+    {cartId: 3, productId: 3, name: '상품 3', price: 20000, quantity: 1},
   ]);
 
-  const increaseQuantity = (cartId, productId) => {
-    const updatedProducts = items.map((cart) => {
-      if (cart.cartId === cartId && cart.productId === productId) {
-        setCart(cartId, true);
-        fetch('http://localhost:8080/users/carts', {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-          },
-          body: JSON.stringify(cart),
-        });
-        return { ...cart, quantity: cart.quantity + 1 };
+  const increaseQuantity = (cartId) => {
+    axios({
+      url: "http://localhost:8080/users/carts",
+      method: 'put',
+      data: {
+        cartId: cartId,
+        isIncrease: true
       }
-      return cart;
-    });
-    setItems(updatedProducts);
+    })
   };
 
-  const decreaseQuantity = (cartId, productId) => {
-    const updatedCarts = items.map((cart) => {
-      if (cart.cartId === cartId && cart.productId === productId && cart.quantity > 1) {
-        setCart(productId, false);
-        fetch('http://localhost:8080/users/carts', {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-          },
-          body: JSON.stringify(cart),
-        });
-        return { ...cart, quantity: cart.quantity - 1 };
+  const decreaseQuantity = (cartId) => {
+    axios({
+      url: "http://localhost:8080/users/carts",
+      method: 'put',
+      data: {
+        cartId: cartId,
+        isIncrease: false
       }
-      return cart;
-    });
-    setItems(updatedCarts);
+    })
+  };
+
+  const removeProduct = (cartId) => {
+    axios({
+      url: "http://localhost:8080/users/carts/${cartId}",
+      method: 'delete'
+    })
   };
 
   return (
-    <div className="App">
-      <h1>상품 목록</h1>
-      <table border="1" width="500" height="300" align="center">
-        <thead>
+      <div className="App">
+        <h1>상품 목록</h1>
+        <table border="1" width="500" height="300" align="center">
+          <thead>
           <tr>
             <th>상품</th>
             <th>가격</th>
             <th>수량</th>
             <th>총 가격</th>
           </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
           {items.map((cart) => (
-            <tr key={cart.id}>
-              <td>{cart.name}</td>
-              <td>${cart.price}</td>
-              <td>
-                <button onClick={() => decreaseQuantity(cart.cartId, cart.productId)}>-</button>
-                {cart.quantity}
-                <button onClick={() => increaseQuantity(cart.cartId, cart.productId)}>+</button>
-              </td>
-              <td>${cart.price * cart.quantity}</td>
-            </tr>
+              <tr key={cart.id}>
+                <td>{cart.name}</td>
+                <td>${cart.price}</td>
+                <td>
+                  <button onClick={() => decreaseQuantity(cart.cartId, cart.productId)}>-</button>
+                  {cart.quantity}
+                  <button onClick={() => increaseQuantity(cart.cartId, cart.productId)}>+</button>
+                  <button onClick={() => removeProduct(cart.cartId)}><b>x</b></button>
+                </td>
+                <td>${cart.price * cart.quantity}</td>
+              </tr>
           ))}
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+          <button onClick={() => navigate("/receipt")}><b>주문</b></button>
+        </table>
+
+      </div>
   );
 }
 
