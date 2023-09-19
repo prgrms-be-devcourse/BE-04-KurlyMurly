bad4f8ffbc3dd020c80c6968aeecd7c3b449d751
2jie0516 <dhtpgks10044@gmail.com>
2023-09-19 17:54:00 +0900

    [KM-318] 수정:테스트 이름 수정

--- module-core/src/test/java/com/devcourse/kurlymurly/module/user/domain/UserTest.java
+++ module-core/src/test/java/com/devcourse/kurlymurly/module/user/domain/UserTest.java
@@ -49,7 +49,7 @@ class UserTest {
 
     @Test
     @DisplayName("등급에 따른 적립금 지급 테스트_라벤더")
-    void save_reward_purple_lavender() {
+    void save_reward_lavender() {
         // Given
         Reward reward = new Lavender();
 
@@ -62,7 +62,7 @@ class UserTest {
 
     @Test
     @DisplayName("등급에 따른 적립금 지급 테스트_퍼플")
-    void save_reward_purple_purple() {
+    void save_reward_purple() {
         // Given
         Reward reward = new Purple();
 
