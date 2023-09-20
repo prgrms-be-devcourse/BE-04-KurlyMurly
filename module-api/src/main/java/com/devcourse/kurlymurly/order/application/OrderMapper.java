ebb4c9550e7b101e773cfdeb3b82e11565ea6d9c
sujjangOvO <ssu7038@yu.ac.kr>
2023-09-20 19:48:51 +0900

    수정: mapper 향상된 for문으로 변경

--- module-api/src/main/java/com/devcourse/kurlymurly/order/application/OrderMapper.java
+++ module-api/src/main/java/com/devcourse/kurlymurly/order/application/OrderMapper.java
@@ -3,6 +3,7 @@ package com.devcourse.kurlymurly.order.application;
 import com.devcourse.kurlymurly.module.order.domain.OrderDomain;
 import com.devcourse.kurlymurly.module.order.domain.OrderItem;
 import com.devcourse.kurlymurly.web.dto.order.CreateOrder;
+import com.devcourse.kurlymurly.web.dto.order.CreateOrderItem;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,13 +14,13 @@ public class OrderMapper {
     public OrderDomain toOrderDomain(CreateOrder.Request request) {
         List<OrderItem> orderItems = new ArrayList<>();
 
-        for(int i=0; i<request.orderItems().size(); i++) {
+        for(CreateOrderItem.Request req : request.orderItems()) {
             orderItems.add(
                     new OrderItem(
-                            request.orderItems().get(i).productId(),
-                            request.orderItems().get(i).productName(),
-                            request.orderItems().get(i).totalPrice(),
-                            request.orderItems().get(i).quantity()
+                            req.productId(),
+                            req.productName(),
+                            req.totalPrice(),
+                            req.quantity()
                     )
             );
         }
