624b6c52896e997ac3973cfa6c923407bcdf2ded
2jie0516 <dhtpgks10044@gmail.com>
2023-09-25 00:23:08 +0900

    [KM-323] 수정:결제수단 조회 로직 수정(쿼리 , 예와)

--- module-core/src/main/java/com/devcourse/kurlymurly/module/user/domain/payment/PaymentRepository.java
+++ module-core/src/main/java/com/devcourse/kurlymurly/module/user/domain/payment/PaymentRepository.java
@@ -1,9 +1,15 @@
 package com.devcourse.kurlymurly.module.user.domain.payment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface PaymentRepository extends JpaRepository<Payment,Long> {
-    Optional<Payment> findByUserIdAndId(Long userId,Long paymentId);
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByUserIdAndId(Long userId, Long paymentId);
+
+    @Query("SELECT e FROM Payment e WHERE e.userId = :userId AND (e.status = 'DEFAULT' OR e.status = 'NORMAL')")
+    List<Payment> findByUserIdAndStatus(@Param("userId") Long userId);
 }
