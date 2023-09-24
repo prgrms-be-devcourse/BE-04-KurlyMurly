0dc52908ac602f137ab8e836afa62e77c0eeba4f
2jie0516 <dhtpgks10044@gmail.com>
2023-09-24 23:55:55 +0900

    [KM-323] 수정:인증이 필요한 API들에 대해 컨트롤러 분리

--- module-api/src/main/java/com/devcourse/kurlymurly/user/MemberController.java
+++ module-api/src/main/java/com/devcourse/kurlymurly/user/MemberController.java
@@ -1,5 +1,6 @@
 package com.devcourse.kurlymurly.user;
 
+<<<<<<<< HEAD:module-api/src/main/java/com/devcourse/kurlymurly/user/MemberController.java
 import com.devcourse.kurlymurly.module.user.domain.User;
 import com.devcourse.kurlymurly.module.user.service.MemberService;
 import com.devcourse.kurlymurly.web.common.KurlyResponse;
@@ -13,29 +14,34 @@ import com.devcourse.kurlymurly.web.dto.user.payment.UpdatePayPassword;
 import com.devcourse.kurlymurly.web.dto.user.shipping.AddAddress;
 import com.devcourse.kurlymurly.web.dto.user.shipping.GetAddress;
 import com.devcourse.kurlymurly.web.dto.user.shipping.UpdateAddress;
+========
+import com.devcourse.kurlymurly.module.user.service.UserService;
+import com.devcourse.kurlymurly.web.common.KurlyResponse;
+import com.devcourse.kurlymurly.web.dto.user.CheckEmail;
+import com.devcourse.kurlymurly.web.dto.user.CheckId;
+import com.devcourse.kurlymurly.web.dto.user.Join;
+import com.devcourse.kurlymurly.web.dto.user.Login;
+>>>>>>>> 87195d7 ([KM-323] 수정:인증이 필요한 API들에 대해 컨트롤러 분리):module-api/src/main/java/com/devcourse/kurlymurly/user/UserController.java
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 import static org.springframework.http.HttpStatus.OK;
 
 @Tag(name = "member", description = "유저 API")
 @RestController
+<<<<<<<< HEAD:module-api/src/main/java/com/devcourse/kurlymurly/user/MemberController.java
 @RequestMapping("/users")
 public class MemberController {
+========
+public class UserController {
+    private final UserService userService;
+>>>>>>>> 87195d7 ([KM-323] 수정:인증이 필요한 API들에 대해 컨트롤러 분리):module-api/src/main/java/com/devcourse/kurlymurly/user/UserController.java
 
     private final MemberService memberService;
 
@@ -43,6 +49,7 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+<<<<<<<< HEAD:module-api/src/main/java/com/devcourse/kurlymurly/user/MemberController.java
     @Tag(name = "member")
     @GetMapping("/reviews")
     @ResponseStatus(OK)
@@ -270,5 +277,54 @@ public class MemberController {
     ) {
         memberService.changeItemQuantity(updateCart.cartId(), updateCart.isIncrease());
         return KurlyResponse.noData();
+========
+    @Tag(name = "user")
+    @Operation(description = "로그인 API", responses = {
+            @ApiResponse(responseCode = "200", description = "로그인에 성공한 경우"),
+            @ApiResponse(responseCode = "422", description = "잘못된 로그인 정보를 입력한 경우"),
+    })
+    @PostMapping("/login")
+    @ResponseStatus(OK)
+    public KurlyResponse<Login.Response> login(@RequestBody @Valid Login.Request request) {
+        Login.Response response = userService.login(request.loginId(), request.password());
+
+        return KurlyResponse.ok(response);
+    }
+
+    @Tag(name = "user")
+    @Operation(description = "회원 가입 API", responses = {
+            @ApiResponse(responseCode = "200", description = "회원 가입에 성공한 경우"),
+            @ApiResponse(responseCode = "400", description = "변경할 비밀번호와 확인 비밀번호가 일치하지 않는 경우"),
+            @ApiResponse(responseCode = "409", description = "이미 가입된 아이디를 입력한 경우"),
+            @ApiResponse(responseCode = "409", description = "이미 가입된 이메일을 입력한 경우")
+    })
+    @PostMapping("/signUp")
+    @ResponseStatus(OK)
+    public KurlyResponse<Void> join(@RequestBody @Valid Join.Request request) {
+        userService.join(request);
+        return KurlyResponse.noData();
+    }
+
+    @Tag(name = "user")
+    @Operation(description = "id 중복 체크 API", responses = {
+            @ApiResponse(responseCode = "200", description = "해당 id의 중복검사를 진행"),
+    })
+    @PostMapping("/check-id")
+    @ResponseStatus(OK)
+    public KurlyResponse<Void> checkId(@RequestBody @Valid CheckId.Request request) {
+        boolean result = userService.checkId(request.loginId());
+        return KurlyResponse.ok(result);
+    }
+
+    @Tag(name = "user")
+    @Operation(description = "이메일 중복 체크 API", responses = {
+            @ApiResponse(responseCode = "200", description = "해당 이메일에 대한 중복검사를 진행")
+    })
+    @PostMapping("/check-email")
+    @ResponseStatus(OK)
+    public KurlyResponse<Void> checkEmail(@RequestBody @Valid CheckEmail.Request request) {
+        boolean result = userService.checkEmail(request.email());
+        return KurlyResponse.ok(result);
+>>>>>>>> 87195d7 ([KM-323] 수정:인증이 필요한 API들에 대해 컨트롤러 분리):module-api/src/main/java/com/devcourse/kurlymurly/user/UserController.java
     }
 }
