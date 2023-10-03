37290fca1025632994d3065719240c8dffb1e4a1
2jie0516 <dhtpgks10044@gmail.com>
2023-10-04 00:23:28 +0900

    추가:유저 도메인 ddl 작성

---
+++ kurly-domain/kurly-db/src/main/resources/sql/user-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(name, login_id, password, tier, reward, email, sex, birth, phone_number,
+                  role, status)
+VALUES ("수연장", "abd1234", "kurly1234", "PURPLE", 0, "aaaa@gmail.com","male", DATE('2022-06-11 12:34:56'), "1234-4567","ROLE_USER","NORMAL")
